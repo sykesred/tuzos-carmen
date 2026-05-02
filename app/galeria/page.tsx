3df446b18ae6597c9f9968cbd5bb3abc2dc6a6e6
Mafer Rodriguez <mafernanda.rodriguezt.dev@gmail.com>
2026-05-02 15:17:50 -0600

    prueba galeria

--- app/galeria/page.tsx
+++ app/galeria/page.tsx
@@ -9,62 +9,62 @@ export default function GaleriaPage() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {/* Ejemplo */}
         <img
-          src="/galeria/tuzos1.jpg"
+          src="/galeria/tuzos1"
           alt="Torneo UFD bienvenida"
           className="w-full h-64 object-cover rounded-2xl"
         />
         <img
-          src="/galeria/tuzos2.jpg"
+          src="/galeria/tuzos2"
           alt="Primeras visorias en Tuzos Carmen"
           className="w-full h-64 object-cover rounded-2xl"
         />
         <img
-          src="/galeria/tuzos3.jpg"
+          src="/galeria/tuzos3"
           alt="Categoría Genuine en Tuzos Carmen"
           className="w-full h-64 object-cover rounded-2xl"
         />
          <img
-          src="/galeria/tuzos4.jpg"
+          src="/galeria/tuzos4"
           alt="Categopría Genuine en Tuzos Carmen"
           className="w-full h-64 object-cover rounded-2xl"
         />
          <img
-          src="/galeria/tuzos5.jpg"
+          src="/galeria/tuzos5"
           alt="Equipo Tuzos Carmen en torneo UFD"
           className="w-full h-64 object-cover rounded-2xl"
         />
          <img
-          src="/galeria/tuzos6.jpg"
+          src="/galeria/tuzos6"
           alt="Mascotas Tuzos Pachuca"
           className="w-full h-64 object-cover rounded-2xl"
         />
          <img
-          src="/galeria/tuzos7.jpg"
+          src="/galeria/tuzos7"
           alt="Entrenamiento en Tuzos Carmen"
           className="w-full h-64 object-cover rounded-2xl"
         />
          <img
-          src="/galeria/tuzos8.jpg"
+          src="/galeria/tuzos8"
           alt="Equipo Tuzos Carmen en estadio Hidalgo"
           className="w-full h-64 object-cover rounded-2xl"
         />
          <img
-          src="/galeria/tuzos9.jpg"
+          src="/galeria/tuzos9"
           alt="Equipo Tuzos Carmen en torneo UFD"
           className="w-full h-64 object-cover rounded-2xl"
         />
          <img
-          src="/galeria/tuzos10.jpg"
+          src="/galeria/tuzos10"
           alt="Equipo Tuzos Carmen en estadio Hidalgo"
           className="w-full h-64 object-cover rounded-2xl"
         />
          <img
-          src="/galeria/tuzos11.jpg"
+          src="/galeria/tuzos11"
           alt="Tuzito en torneo UFD"
           className="w-full h-64 object-cover rounded-2xl"
         />
          <img
-          src="/galeria/tuzos12.jpg"
+          src="/galeria/tuzos12"
           alt="Calentamiento para torneo UFD"
           className="w-full h-64 object-cover rounded-2xl"
         />
