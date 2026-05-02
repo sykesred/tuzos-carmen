acc64866627cda78ee1b9eaf26ad672201af4702
Mafer Rodriguez <mafernanda.rodriguezt.dev@gmail.com>
2026-05-02 15:24:47 -0600

    jjj

--- app/galeria/page.tsx
+++ app/galeria/page.tsx
@@ -1,17 +1,17 @@
 export default function GaleriaPage() {
   const images = [
-    { src: "/galeria/tuzos1.jpg", alt: "Torneo UFD bienvenida" },
-    { src: "/galeria/tuzos2.jpg", alt: "Primeras visorías en Tuzos Carmen" },
-    { src: "/galeria/tuzos3.jpg", alt: "Categoría Genuine en Tuzos Carmen" },
-    { src: "/galeria/tuzos4.jpg", alt: "Categoría Genuine en Tuzos Carmen" },
-    { src: "/galeria/tuzos5.jpg", alt: "Equipo Tuzos Carmen en torneo UFD 2026" },
-    { src: "/galeria/tuzos6.jpg", alt: "Mascotas Tuzos Pachuca" },
-    { src: "/galeria/tuzos7.jpg", alt: "Equipo Tuzos Carmen entrenando" },
-    { src: "/galeria/tuzos8.jpg", alt: "Equipo Tuzos Carmen en Estadio Hidalgo" },
-    { src: "/galeria/tuzos9.jpg", alt: "Equipo Tuzos en torneo UFD 2026" },
-    { src: "/galeria/tuzos10.jpg", alt: "Equipo Tuzos Carmen en Estadio Hidalgo" },
-    { src: "/galeria/tuzos11.jpg", alt: "Tuzito en torneo UFD 2026" },
-    { src: "/galeria/tuzos12.jpg", alt: "Equipo Tuzos Carmen entrenando para torneo UFD 2026" },
+    { src: "/images/galeria/tuzos1.jpg", alt: "Torneo UFD bienvenida" },
+    { src: "/images/galeria/tuzos2.jpg", alt: "Primeras visorías en Tuzos Carmen" },
+    { src: "/images/galeria/tuzos3.jpg", alt: "Categoría Genuine en Tuzos Carmen" },
+    { src: "/images/galeria/tuzos4.jpg", alt: "Categoría Genuine en Tuzos Carmen" },
+    { src: "/images/galeria/tuzos5.jpg", alt: "Equipo Tuzos Carmen en torneo UFD 2026" },
+    { src: "/images/galeria/tuzos6.jpg", alt: "Mascotas Tuzos Pachuca" },
+    { src: "/images/galeria/tuzos7.jpg", alt: "Equipo Tuzos Carmen entrenando" },
+    { src: "/images/galeria/tuzos8.jpg", alt: "Equipo Tuzos Carmen en Estadio Hidalgo" },
+    { src: "/images/galeria/tuzos9.jpg", alt: "Equipo Tuzos en torneo UFD 2026" },
+    { src: "/images/galeria/tuzos10.jpg", alt: "Equipo Tuzos Carmen en Estadio Hidalgo" },
+    { src: "/images/galeria/tuzos11.jpg", alt: "Tuzito en torneo UFD 2026" },
+    { src: "/images/galeria/tuzos12.jpg", alt: "Equipo Tuzos Carmen entrenando para torneo UFD 2026" },
   ]
 
   return (
