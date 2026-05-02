4f60be126f1b5e5108f3545e17cdbcbd259c1c6b
gramirezOreico <gramirez@oreico.net>
2026-05-01 21:40:30 -0600

    Cards acomodadas

--- components/home-overview.tsx
+++ components/home-overview.tsx
@@ -61,7 +61,7 @@ export function HomeOverview() {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5">
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-5 max-w-4xl mx-auto">
           {cards.map(({ icon: Icon, href, color, bg, border, titleEs, titleEn, descEs, descEn }) => (
             <Link
               key={href}
